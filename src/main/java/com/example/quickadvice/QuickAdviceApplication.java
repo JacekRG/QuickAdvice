523be0ffaa82b5b2c7ecaf53d954a711a8d6d9b1
Jacek <steiner@10g.pl>
2023-03-13 15:05:13 +0100

    first commit

---
+++ src/main/java/com/example/quickadvice/QuickAdviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickadvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickAdviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickAdviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quickadvice/QuickAdviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickadvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickAdviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
