6d5f3295fada679c65b109a735b4623344ae261b
Jacek <steiner@10g.pl>
2023-03-26 13:05:35 +0200

    QuestionViewController created

---
+++ src/main/java/com/example/quickadvice/question/controller/QuestionViewController.java
@@ -0,0 +1,93 @@
+package com.example.quickadvice.question.controller;
+
+import com.example.quickadvice.QuickAdviceConfiguration;
+import com.example.quickadvice.category.service.CategoryService;
+import com.example.quickadvice.common.controller.QuickAdviceCommonViewController;
+import com.example.quickadvice.question.domain.model.Question;
+import com.example.quickadvice.question.service.AnswerService;
+import com.example.quickadvice.question.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+import static com.example.quickadvice.common.controller.ControllerUtils.paging;
+
+@Controller
+@RequestMapping("/questions")
+@RequiredArgsConstructor
+public class QuestionViewController extends QuickAdviceCommonViewController {
+
+	private final QuestionService questionService;
+	private final AnswerService answerService;
+	private final CategoryService categoryService;
+	private final QuickAdviceConfiguration quickAdviceConfiguration;
+
+	@GetMapping
+	public String indexView(Model model){
+		model.addAttribute("questions", questionService.getQuestions());
+		addGlobalAttributes(model);
+
+		return "question/index";
+	}
+
+	@GetMapping("{id}")
+	public String singleView(Model model, @PathVariable UUID id){
+		model.addAttribute("question", questionService.getQuestion(id));
+		model.addAttribute("answers", answerService.getAnswers(id));
+		addGlobalAttributes(model);
+
+		return "question/single";
+	}
+
+	@GetMapping("add")
+	public String addView(Model model){
+		model.addAttribute("question", new Question());
+
+		return "question/add";
+	}
+
+	@PostMapping
+	public String add(Question question){
+		questionService.createQuestion(question);
+
+		return "redirect:/questions";
+	}
+
+	@GetMapping("hot")
+	public String hotView(
+			@RequestParam(name = "page", defaultValue = "1") int page,
+			Model model
+	){
+		PageRequest pageRequest = PageRequest.of(page - 1, quickAdviceConfiguration.getPagingPageSize());
+
+		Page<Question> questionsPage = questionService.findHot(pageRequest);
+
+		model.addAttribute("questionsPage", questionsPage);
+		paging(model, questionsPage);
+		addGlobalAttributes(model);
+
+		return "question/index";
+	}
+
+	@GetMapping("unanswered")
+	public String unansweredView(
+			@RequestParam(name = "page", defaultValue = "1") int page,
+			Model model
+	){
+		PageRequest pageRequest = PageRequest.of(page - 1, quickAdviceConfiguration.getPagingPageSize());
+
+		Page<Question> questionsPage = questionService.findUnanswered(pageRequest);
+
+		model.addAttribute("questionsPage", questionsPage);
+		paging(model, questionsPage);
+		addGlobalAttributes(model);
+
+		return "question/index";
+	}
+
+}
