db02b632dc09ffb4d61145d776b269ec683d9d4b
Jacek <steiner@10g.pl>
2023-03-26 13:00:16 +0200

    AnswerService extended

--- src/main/java/com/example/quickadvice/question/service/AnswerService.java
+++ src/main/java/com/example/quickadvice/question/service/AnswerService.java
@@ -1,35 +1,62 @@
 package com.example.quickadvice.question.service;
 
 import com.example.quickadvice.question.domain.model.Answer;
+import com.example.quickadvice.question.domain.model.Question;
+import com.example.quickadvice.question.domain.repository.AnswerRepository;
+import com.example.quickadvice.question.domain.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class AnswerService {
 
+    private final AnswerRepository answerRepository;
+
+    private final QuestionRepository questionRepository;
+
+    public AnswerService(AnswerRepository answerRepository, QuestionRepository questionRepository) {
+        this.answerRepository = answerRepository;
+        this.questionRepository = questionRepository;
+    }
+
+    @Transactional(readOnly = true)
     public List<Answer> getAnswers(UUID questionId) {
-        return Arrays.asList(
-                new Answer("Answer 1"),
-                new Answer("Answer 2")
-        );
+        return answerRepository.findByQuestionId(questionId);
     }
 
+    @Transactional(readOnly = true)
     public Answer getAnswer(UUID id) {
-        return new Answer("Answer");
+        return answerRepository.getById(id);
     }
 
-    public Answer createAnswer(UUID questionId, Answer answer) {
-        answer.setId(UUID.randomUUID());
+    @Transactional
+    public Answer createAnswer(UUID questionId, Answer answerRequest) {
+        Answer answer = new Answer();
+
+        answer.setName(answerRequest.getName());
+
+        Question question = questionRepository.getById(questionId);
+        question.addAnswer(answer);
+
+        answerRepository.save(answer);
+        questionRepository.save(question);
+
         return answer;
     }
 
-    public Answer updateAnswer(UUID id, Answer answer) {
-        return answer;
+    @Transactional
+    public Answer updateAnswer(UUID answerId, Answer answerRequest) {
+        Answer answer = answerRepository.getById(answerId);
+        answer.setName(answerRequest.getName());
+
+        return answerRepository.save(answer);
     }
 
-    public void deleteAnswer(UUID id) {
+    @Transactional
+    public void deleteAnswer(UUID answerId) {
+        answerRepository.deleteById(answerId);
     }
 }
